7989c64ddfa612addd05bc7369e4ea9b37271552
deleo <deleo@LAPTOP-1Q15UABM>
2025-11-28 16:22:08 -0600

    Yo

--- Unidad4_Noticias/Controllers/HomeController.cs
+++ Unidad4_Noticias/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Unidad4_Noticias.Models.ViewModels;
 using Unidad4_Noticias.Services;
 
@@ -29,6 +30,21 @@ namespace Unidad4_Noticias.Controllers
         {
             var vm = noticiasService.GetNoticiaDetails(id);
             return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Comentar(DetailsViewModel vm)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var id = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value??"0");
+                noticiasService.AgregarComentario(vm.Id, vm.NuevoComentario?.Contenido??"", id);
+            }
+            else
+            {
+                noticiasService.AgregarComentario(vm.Id, vm.NuevoComentario?.Contenido ?? "", null);
+
+            }
+            return RedirectToAction("Details", new { id = vm.Id });
         }
     }
 }
